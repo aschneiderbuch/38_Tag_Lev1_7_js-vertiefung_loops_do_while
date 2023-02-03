f9a4222ee1cf1227ed25b75025c73b0d8b624f65
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 13:27:06 +0100

    doWhileVsFor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
 console.log("test");
+
+/* 
+
+Frage
+
+do...while loop
+
+ausgabe in html
+    The number is 1 
+    ...
+    The number is 5
+*/
+
+let runden = 0;
+
+do { 
+    runden = runden + 1;
+    document.write(`DoWile: The number is ${runden} <br>`);
+
+}while(runden < 5);
+
+
+
+for (runden = 1; runden < 6; runden = runden + 1){
+    document.write(`For: The number is ${runden} <br>`);
+}
